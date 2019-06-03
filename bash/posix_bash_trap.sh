55c82d63c32efd73df4165acaf5563a0a343a49b
Yoki Yang <yoki.yang@ingenico.com>
2019-06-03 16:32:02 -0400

    20190603 Add pass by two arguments

--- bash/posix_bash_trap.sh
+++ bash/posix_bash_trap.sh
@@ -31,3 +31,24 @@ trap cleanup ERR
 
 # Run everytime finished the scripts
 trap cleanup EXIT
+
+
+## Pass Multiple Arguments
+docker_cleanup () {
+    set +e
+    local loc_image_name_1=$1;
+    local loc_image_name_2=$2;
+    echo ""
+    echo ""
+    echo ">>>>>>>> Starting the final cleanup scripts  >>>>>>>>>>>>>>>"
+    echo ""
+	echo "# !!! PLEASE IGNORE WARNINGS IF IMAGES NOT FOUND !!!"
+    docker images -a |  grep "projectKeyWords" | grep "${loc_image_name_1}"
+    docker images -a |  grep "projectKeyWords" | grep "${loc_image_name_1}" | awk '{print $3}' | xargs docker rmi -f
+    docker images -a |  grep "projectKeyWords" | grep "${loc_image_name_2}"
+    docker images -a |  grep "projectKeyWords" | grep "${loc_image_name_2}" | awk '{print $3}' | xargs docker rmi -f
+    set -e
+}
+## ============== END: FUNCTION DEFINITIONS =================
+
+trap 'docker_cleanup ${application_name_1} ${application_name_2}' ERR ## Call docker_cleanup whenever the script exits with or without failure
